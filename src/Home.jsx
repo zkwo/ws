318d4dd10dac4cbc42c6a49caf18b9a272ad7cc8
zkwo <ko40c980ew632@gmail.com>
2026-04-02 16:57:15 +0700

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import FaqItem from '../components/FaqItem';
+
+export default function Home({ isDarkMode, setIsDarkMode }) {
+  return (
+    <div className="min-h-screen">
+      <Navbar darkMode={isDarkMode} setDarkMode={setIsDarkMode} />
+      {/* Isi konten landing page kamu di sini */}
+      <div className="max-w-md mx-auto px-6 mt-10">
+         <h1 className="text-4xl font-bold text-center">VALTUS</h1>
+         <p className="text-center opacity-50">Premium Robux Store</p>
+      </div>
+      <Footer />
+    </div>
+  );
+}
