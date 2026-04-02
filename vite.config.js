60255ef58689b441419d1a8ebf4466acbd15c568
zkwo <ko40c980ew632@gmail.com>
2026-04-02 18:41:39 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // Ini memaksa Vite untuk mengecek ekstensi jika kamu lupa menulisnya
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json']
+  },
+  build: {
+    // Memastikan folder outputnya sesuai standar Vercel
+    outDir: 'dist',
+  }
 })
